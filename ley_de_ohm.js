551c6256baf26fafb8ee397b323a767d7f6e6197
asdjosegarcia <asdjosegarcia@gmail.com>
2023-09-10 20:13:21 -0300

    fix responsive css add styles add mah buton

--- ley_de_ohm.js
+++ ley_de_ohm.js
@@ -1,3 +1,5 @@
+////////////////////////////////////////////////////global variables
+let mahIs=false;
 /////////////////////////////////////////////////////voltageCalc
 const streamCalcVolt = document.querySelector('#input-stream-calc-voltage')
 const resitenceCalcVolt = document.querySelector('#input-resistence-calc-voltage')
@@ -8,15 +10,18 @@ const voltageCalcStream = document.querySelector('#input-voltage-calc-stream')
 const resistenceCalcStream = document.querySelector('#input-resistence-calc-stream')
 const btnCalcStream = document.querySelector('#btn-calc-stream')
 const resultCalcStreamSpan = document.querySelector('#calc-stream-result')
+const btnCalcMah=document.querySelector('#btn-calc-mah')
 //////////////////////////////////////////////////////resitenceCalc
 const voltageCalcResistence = document.querySelector('#input-voltage-calc-resistence')
 const streamCalcResistence = document.querySelector('#input-stream-calc-resistence')
 const btnCalcResistence = document.querySelector('#btn-calc-reistence')
 const resultCalcResistenceSpan = document.querySelector('#calc-resistence-result')
 
+
 btnCalcVolt.addEventListener('click', calcVoltage)
 btnCalcStream.addEventListener('click', calcStream)
 btnCalcResistence.addEventListener('click', calcResistence)
+btnCalcMah.addEventListener('click',mahToggle)
 
 
 function calcVoltage() {
@@ -24,21 +29,30 @@ function calcVoltage() {
     const stream = streamCalcVolt.value
     console.log('resitencia', resistence, 'corriente', stream);
     voltage = stream * resistence;
-    console.log(voltage);
-    resultCalcVoltageSpan.innerText = voltage + ' V'
-    return voltage
+    resultCalcVoltageSpan.innerText = voltage.toFixed(2) + ' V'
 }
 
+
+function mahToggle(){
+    mahIs=!mahIs
+    calcStream(mahIs)
+}
 function calcStream() {
     const voltage = voltageCalcStream.value
     const resistence = resistenceCalcStream.value
-    stream = voltage / resistence;
-    resultCalcStreamSpan.innerText = stream + ' A'
+    stream = Number(voltage / resistence);
+    if(mahIs){
+        const  mah=stream*1000;
+        resultCalcStreamSpan.innerText = mah.toFixed(2) + ' mAh'
+    }else{
+        resultCalcStreamSpan.innerText = stream.toFixed(4) + ' A'   
+    }
+
 }
 
 function calcResistence() {
     const voltage = voltageCalcResistence.value
     const stream = streamCalcResistence.value
     resistence = voltage / stream
-    resultCalcResistenceSpan.innerText = resistence + ' Ω'
+    resultCalcResistenceSpan.innerText = resistence.toFixed(2)   + ' Ω'
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+const test=0.009
+
+console.log('miliampers '+(test*1000))
+// console.log(Math.pow((test*10),3))
