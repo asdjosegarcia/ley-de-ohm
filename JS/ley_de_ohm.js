7be3adcc248fab29ce5f68062902df306cbb2606
asdjosegarcia <asdjosegarcia@gmail.com>
2023-10-03 12:25:21 -0300

    change hidde calculate-btns change variables outputs add auto calculate variables fix responsive styles

--- JS/ley_de_ohm.js
+++ JS/ley_de_ohm.js
@@ -1,22 +1,22 @@
 ////////////////////////////////////////////////////global variables
-let calcVoltMah=false;
-let calcResMah=false;
-let calcStreamMah=false;
+let calcVoltMah = false;
+let calcResMah = false;
+let calcStreamMah = false;
 /////////////////////////////////////////////////////voltageCalc
 const streamCalcVolt = document.querySelector('#input-stream-calc-voltage')//input corriente
 const resitenceCalcVolt = document.querySelector('#input-resistance-calc-voltage')//input resitencia
 const btnCalcVolt = document.querySelector('#btn-calc-volt')//boton "calcular"
 const resultCalcVoltageSpan = document.querySelector('#calc-voltage-result')//span donde se muestra el resultado
 const btnCalcVoltMah = document.querySelector('#btn-calc-volt-mah')//boton "mAh" que alterna entre miliampers y ampers
-const matOperationVolt=document.querySelector('#mathematical-operation-voltage') //span que contiene la operacion matematica que se realiza
+const matOperationVolt = document.querySelector('#mathematical-operation-voltage') //span que contiene la operacion matematica que se realiza
 const ampMahSymbolVoltage = document.querySelector('#Amp-mAh-symbol-voltage')//span que varia entre A y mAh
 /////////////////////////////////////////////////////streamCalc
 const voltageCalcStream = document.querySelector('#input-voltage-calc-stream')//input voltaje
 const resistanceCalcStream = document.querySelector('#input-resistance-calc-stream')//input resistencia
 const btnCalcStream = document.querySelector('#btn-calc-stream')//boton calcular corriente
 const resultCalcStreamSpan = document.querySelector('#calc-stream-result')//span que nos da el resultado
-const btnCalStreamcMah=document.querySelector('#btn-calc-stream-mah')//boton que alterna tente mAh y A
-const matOperationStream=document.querySelector('#mathematical-operation-stream') //span que contiene la operacion matematica que se realiza
+const btnCalStreamcMah = document.querySelector('#btn-calc-stream-mah')//boton que alterna tente mAh y A
+const matOperationStream = document.querySelector('#mathematical-operation-stream') //span que contiene la operacion matematica que se realiza
 
 //////////////////////////////////////////////////////resitenceCalc
 const voltageCalcResistance = document.querySelector('#input-voltage-calc-resistance')//input voltaje
@@ -24,7 +24,7 @@ const streamCalcResistance = document.querySelector('#input-stream-calc-resistan
 const btnCalcResistance = document.querySelector('#btn-calc-reistance')//boton calcular
 const resultCalcResistanceSpan = document.querySelector('#calc-resistance-result')//span resultado
 const btnCalcResMah = document.querySelector('#btn-calc-res-mah')//boton "mAh" que alterna entre miliampers y ampers
-const matOperationResistance=document.querySelector('#mathematical-operation-resistance') //span que contiene la operacion matematica que se realiza
+const matOperationResistance = document.querySelector('#mathematical-operation-resistance') //span que contiene la operacion matematica que se realiza
 const ampMahSimbolResistance = document.querySelector('#Amp-mAh-symbol-resistance')//span que varia entre A y mAh
 
 
@@ -33,27 +33,32 @@ const ampMahSimbolResistance = document.querySelector('#Amp-mAh-symbol-resistanc
 btnCalcVolt.addEventListener('click', calcVoltage)
 btnCalcStream.addEventListener('click', calcStream)
 btnCalcResistance.addEventListener('click', calcResistance)
-btnCalcVoltMah.addEventListener('click',  ()=>{calcVoltMah=!calcVoltMah;calcVoltage()})//cambia el valor a true o false para que se divia entre 1000 los ampers
-btnCalcResMah.addEventListener('click',  ()=>{calcResMah=!calcResMah;calcResistance()})//
-btnCalStreamcMah.addEventListener('click', ()=>{calcStreamMah=!calcStreamMah;calcStream()})
+btnCalcVoltMah.addEventListener('click', () => { calcVoltMah = !calcVoltMah; calcVoltage() })//cambia el valor a true o false para que se divia entre 1000 los ampers
+btnCalcResMah.addEventListener('click', () => { calcResMah = !calcResMah; calcResistance() })//
+btnCalStreamcMah.addEventListener('click', () => { calcStreamMah = !calcStreamMah; calcStream() })
 
 
 function calcVoltage() {
     const resistance = resitenceCalcVolt.value
     const stream = streamCalcVolt.value
-    voltage = stream * resistance;
+    // console.log(stream)
+    if (!stream == "" && !resistance == "") {
+        voltage = stream * resistance;
 
-    if(calcVoltMah){
-        voltage=voltage/1000
-        matOperationVolt.textContent='/1000 X '
-        ampMahSymbolVoltage.textContent='mAh'
-        btnCalcVoltMah.textContent='A'
-    }else{
-        matOperationVolt.textContent='X'
-        ampMahSymbolVoltage.textContent='A'
-        btnCalcVoltMah.textContent='mAh'
+        if (calcVoltMah) {
+            voltage = voltage / 1000
+            matOperationVolt.textContent = '/1000 X '
+            ampMahSymbolVoltage.textContent = 'mAh'
+            btnCalcVoltMah.textContent = 'A'
+        } else {
+            matOperationVolt.textContent = 'X'
+            ampMahSymbolVoltage.textContent = 'A'
+            btnCalcVoltMah.textContent = 'mAh'
+        }
+        resultCalcVoltageSpan.innerText = voltage.toFixed(2) + ' V'
+    } else {
+        resultCalcVoltageSpan.innerText = '- - - - V'
     }
-    resultCalcVoltageSpan.innerText = voltage.toFixed(2) + ' V'
 }
 
 
@@ -61,36 +66,44 @@ function calcVoltage() {
 function calcStream() {
     const voltage = voltageCalcStream.value
     const resistance = resistanceCalcStream.value
-    stream = Number(voltage / resistance);
-    if(calcStreamMah){
-        stream=stream*1000
-        resultCalcStreamSpan.innerText = stream.toFixed(2) + ' mAh'
-        // ampMahSymbolStream.textContent='mAh'
-        matOperationStream.textContent='X1000 /'
-        btnCalStreamcMah.textContent='A'
-    }else{
-        resultCalcStreamSpan.innerText = stream.toFixed(4) + ' A'   
-        matOperationStream.textContent='/'
-        btnCalStreamcMah.textContent='mAh'
-
-
+    if (!voltage == "" && !resistance == "") {
+        stream = Number(voltage / resistance);
+        if (calcStreamMah) {
+            stream = stream * 1000
+            resultCalcStreamSpan.innerText = stream.toFixed(2) + ' mAh'
+            // ampMahSymbolStream.textContent='mAh'
+            matOperationStream.textContent = 'X1000 /'
+            btnCalStreamcMah.textContent = 'A'
+        } else {
+            resultCalcStreamSpan.innerText = stream.toFixed(4) + ' A'
+            matOperationStream.textContent = '/'
+            btnCalStreamcMah.textContent = 'mAh'
+        }
+    } else {
+        resultCalcStreamSpan.innerText = (calcStreamMah) ? '- - - - A' : '- - - - mAh'
+        btnCalStreamcMah.innerText = (calcStreamMah) ? 'mAh' : 'A'
     }
 }
 
 function calcResistance() {
     const voltage = voltageCalcResistance.value
     const stream = streamCalcResistance.value
-    resistance = voltage / stream
-    if(calcResMah){
-        resistance=resistance*1000
-        ampMahSimbolResistance.textContent='mAh'
-        matOperationResistance.textContent='X1000 /'
-        btnCalcResMah.textContent='A'
+    if (!stream == "" && !voltage == "") {
+        resistance = voltage / stream
+        if (calcResMah) {
+            resistance = resistance * 1000
+            ampMahSimbolResistance.textContent = 'mAh'
+            matOperationResistance.textContent = 'X1000 /'
+            btnCalcResMah.textContent = 'A'
+        } else {
+            ampMahSimbolResistance.textContent = 'A'
+            matOperationResistance.textContent = '/'
+            btnCalcResMah.textContent = 'mAh'
+        }
+        resultCalcResistanceSpan.innerText = resistance.toFixed(2) + ' Ω'
     }else{
-        ampMahSimbolResistance.textContent='A'
-        matOperationResistance.textContent='/'
-        btnCalcResMah.textContent='mAh'
+        resultCalcResistanceSpan.innerText = '- - - - Ω'
     }
-    resultCalcResistanceSpan.innerText = resistance.toFixed(2)   + ' Ω'
+
 }
 
